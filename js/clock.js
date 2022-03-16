4542c95260a5dc7dbd0c8313b5fbe8bb5f589e8b
jay1coding <jay1coding@gmail.com>
2022-03-16 15:21:02 +0900

    8day done

---
+++ js/clock.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const clock = document.getElementById('clock');
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const second = String(date.getSeconds()).padStart(2, '0');
+  clock.innerText = `${hours} : ${minutes} : ${second}`;
+}
+
+getClock();
+setInterval(() => {
+  getClock();
+}, 1000);
